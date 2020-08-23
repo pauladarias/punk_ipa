1fc92779935d44d1e8e97d28421c9a8b48668ca7
pauladarias <pauladarias@gmail.com>
2020-08-23 10:21:12 +0100

    Add fetch and show beer names

--- index.js
+++ index.js
@@ -1,29 +1,24 @@
-// variables
 const urlBase = "https://api.punkapi.com/v2/beers";
-
-// Create an async function called "getBeers" that uses fetch to get our beer data from the urlBase.
-// Render each beer name inside the div with the class of beers that currently exists in the HTML file.
 
 async function getBeers() {
   const beerPromise = await fetch(urlBase);
   const beersJson = await beerPromise.json();
 
-  name = beersJson.name;
-  console.log(beersJson[0].name);
+  const beersDiv = document.querySelector(".beers");
 
-  document.querySelector(".beers").innerHTML = name;
+  let html = "";
 
-  // let html = "";
+  beersJson.forEach((beer) => {
+    const name = beer.name;
 
-  // beersJson.forEach((beer) => {
-  //   const name = beer.name;
+    html += `
+     <div class="beers">
+     <h4> ${name} </h4>
+     </div>
+   `;
+  });
 
-  // html *= `
-  //   <div class="beers">
-  //   <h4> ${name} </h4>
-  //   </div>
-  //   `;
-  // });
+  beersDiv.innerHTML = html;
 }
 
 getBeers();
