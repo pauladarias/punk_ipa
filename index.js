62f992949bdd1473605a198048c2a1503c4d77aa
pauladarias <pauladarias@gmail.com>
2020-08-24 09:29:28 +0100

    Add info to the html from json data

--- index.js
+++ index.js
@@ -3,18 +3,28 @@ const urlBase = "https://api.punkapi.com/v2/beers";
 async function getBeers() {
   const beerPromise = await fetch(urlBase);
   const beersJson = await beerPromise.json();
+  console.log(beersJson);
 
   const beersDiv = document.querySelector(".beers");
 
   let html = "";
 
   beersJson.forEach((beer) => {
-    const name = beer.name;
-
     html += `
-     <div class="beers">
-     <h4> ${name} </h4>
-     </div>
+    <div class="beer-wrapper card">
+      <div class="beer">
+        <img class="beer__img" src=${beer.image_url}
+        <h3> ${beer.name} </h3>
+        <span class="beer__info">
+          <span>ABV: ${beer.abv}%</span>
+          <span>IBU: ${beer.ibu}</span>
+        </span>
+        </div>  
+    </div>
+
+
+
+
    `;
   });
 
